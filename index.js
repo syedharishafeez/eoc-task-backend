1a243559d0ac74bf39fc43279b05a435cd0b14b7
syedharishafeez <syed_haris98@yahoo.com>
2020-10-10 23:17:49 +0500

    Add cors

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(cors());
+
 let mysql = require("mysql");
 
 let connection;
